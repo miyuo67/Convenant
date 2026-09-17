51c8fcbb7ce62763156178653da7c29f38830f1a
danielcintradelmonico <danielcintra@alunos.utfpr.edu.br>
2026-09-17 19:27:00 -0300

    Adição do campo exibirstatus

--- convenant.c
+++ convenant.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
+void exibirstatus (float vida_jogador, int vida_boss) {
+
+    printf("    Status da Batalha\n");
+    printf("Sua vida: %.2f\n, vida_jogador");
+    printf("Vida do Boss: %.2f\n, vida_boss");
+}
+
+
+
+
+
+
 int main()
 {
+    float vida_jogador = 100;
+    float vida_boss = 200;
+    float dano_boss = 15.0;
     printf("Hello World");
     printf("Daniel entrou no codigo");
     printf("João entrou no codigo");
