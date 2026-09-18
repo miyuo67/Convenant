c79cbda61112bbcb22e89fba5ac4a7c3f39296b4
Mikael André Ortiz Pelegrino <mikaelandre@alunos.utfpr.edu.br>
2026-09-18 14:50:11 -0300

    Adicionando os ataques, nomes dos personagens e etc

--- convenant.c
+++ convenant.c
@@ -2,14 +2,19 @@
 
 void exibirstatus (float vida_jogador, int vida_boss) {
 
-    printf("    Status da Batalha\n");
+    printf("Status da Batalha\n");
     printf("Sua vida: %.2f\n, vida_jogador");
     printf("Vida do Boss: %.2f\n, vida_boss");
 }
 
+void mostrarMenu() {
 
-
-
+    printf("1- Sorcery (15 de dano)");
+    printf("2- Yetzirah (10 de dano)");
+    printf("3- Atziluth (20 de dano)");
+    printf("4- Dura Lex Sed Lex (20 de dano)");
+    printf("Escolha: ");
+}
 
 
 int main()
@@ -17,7 +22,10 @@ int main()
     float vida_jogador = 100;
     float vida_boss = 200;
     float dano_boss = 15.0;
-    printf("Hello World");
+
+    //Fala dos personagens
+    printf("Reinhard: ");
+    printf("Mercurius: ")
     printf("Daniel entrou no codigo");
     printf("João entrou no codigo");
 
