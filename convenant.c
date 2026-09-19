e857a62ad08734e2240d92e5df4b70d0887eef9a
Mikael André Ortiz Pelegrino <mikaelandre@alunos.utfpr.edu.br>
2026-09-19 13:40:11 -0300

    Adicionando um roteiro, fazendo umas funções

--- convenant.c
+++ convenant.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-void exibirstatus (float vida_jogador, int vida_boss) {
+void exibirstatus (float vidajogador, int vidaboss) {
 
     printf("Status da Batalha\n");
-    printf("Sua vida: %.2f\n, vida_jogador");
-    printf("Vida do Boss: %.2f\n, vida_boss");
+    printf("Sua vida: %.2f\n", vidajogador);
+    printf("Vida do Boss: %.2f\n", vidaboss);
 }
 
 void mostrarMenu() {
@@ -16,6 +16,61 @@ void mostrarMenu() {
     printf("Escolha: ");
 }
 
+void batalha(int vidaJogador, int vidaBoss, int turno) {
+
+    int escolha;
+    int danoJogador = 0;
+    int danoBoss = 15;
+
+    if (vidaJogador <= 0) {
+        printf("Derrota.");
+        printf("Reinhard: Foi uma boa luta, meu amigo, mas você ainda não é páreo.");
+
+        return;
+    }
+
+    if (vidaBoss <=0) {
+        printf("Vitória.");
+        printf("Mecurius:....");
+    
+        return;
+    }
+
+    printf("Turno %d:\n,", turno);
+    exibirstatus(vidaJogador, vidaBoss);
+    mostrarMenu();
+
+    scanf("%d", &escolha);
+    switch(escolha) {
+        case 1:
+        danoJogador = 15;
+        printf("");
+        break;
+
+        case 2:
+        danoJogador = 10;
+        printf("");
+        break;
+
+        case 3:
+        danoJogador = 20;
+        printf("");
+        break;
+
+        case 4:
+        danoJogador = 20;
+        printf("");
+        break;
+        
+        default:
+        printf("Escolha inválida. Tente novamente.\n");
+
+        batalha(vidaJogador, vidaBoss, turno);
+        return;
+    }
+
+
+}
 
 int main()
 {
@@ -24,8 +79,8 @@ int main()
     float dano_boss = 15.0;
 
     //Fala dos personagens
-    printf("Reinhard: ");
-    printf("Mercurius: ")
+    printf("Mercurius: Ah… finalmente, o homem que carrega o céu sobre os ombros. ");
+    printf("Reinhard: E você deve ser o homem que se esconde além dele. ");
     printf("Daniel entrou no codigo");
     printf("João entrou no codigo");
 
