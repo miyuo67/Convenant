277a5bd8c576b5109a928aac502e127e9913182b
Mikael André Ortiz Pelegrino <mikaelandre@alunos.utfpr.edu.br>
2026-09-22 15:17:54 -0300

    arrumando alguns erros

--- convenant.c
+++ convenant.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
 
-/*MOSTRA A VIDA DO JOGADOR E DO BOSS*/
+//mostra  vida do jogador e do boss
 void exibirstatus (float vidajogador, float vidaboss) {
 
     printf("\nStatus da Batalha\n");
     printf("\nSua vida: %.2f\n", vidajogador);
-    printf("\nVida do Boss: %.2f\n", vidaboss);
+    printf("\nVida do Reinhard: %.2f\n", vidaboss);
 }
 
-/* Mostra os ataques disponiveis */
+//mostra os ataques disponiveis
 void mostrarMenu() {
 
     printf("\n1- Sorcery (15 de dano)\n");
     printf("\n2- Yetzirah (10 de dano)\n");
     printf("\n3- Atziluth (20 de dano)\n");
     printf("\n4- Dura Lex Sed Lex (20 de dano)\n");
-    printf("\n5- Usar Escudo da Guarda Áurea e se curar (0 de dano, 15 de HP restaurado )\n");
+    printf("\n5- Se defender e se curar (0 de dano, 15 de HP restaurado )\n");
     printf("\nEscolha: \n");
 }
 
 
-/* Funcao recursiva que controla a batalha */
+//funcao recursiva que controla a batalha
 void batalha(float vidaJogador, float vidaboss, int turno) {
 
     int escolha;
@@ -30,7 +30,7 @@ void batalha(float vidaJogador, float vidaboss, int turno) {
     int danoBoss = 15;
 
 
-    /* CASO BASE: jogador foi derrotado */
+    //verifica se o jogador foi derrotado 
     if (vidaJogador <= 0) {
         printf("Derrota.");
         printf("Reinhard: Foi uma boa luta, meu amigo, mas você ainda não é páreo.");
@@ -38,7 +38,7 @@ void batalha(float vidaJogador, float vidaboss, int turno) {
         return;
     }
 
-    /* CASO BASE: Boss foi derrotado */
+    //verifica se o boss foi derrotado 
     if (vidaboss <=0) {
         printf("Vitória.");
         printf("Mecurius:....");
@@ -51,34 +51,34 @@ void batalha(float vidaJogador, float vidaboss, int turno) {
     mostrarMenu();
 
 
-    /* JOGADOR PODE ESCOLHER ENTRE ATACAR E DEFENDER */
+    //escolhas dos turnos
     scanf("%d", &escolha);
     switch(escolha) {
         case 1:
         danoJogador = 15;
-        printf("");
+        printf("Mercurius: Usou Sorcery");
         break;
 
         case 2:
         danoJogador = 10;
-        printf("");
+        printf("Mercurius: Usou Yetzirah");
         break;
 
         case 3:
         danoJogador = 20;
-        printf("");
+        printf("Mercurius: Usou Atziluth");
         break;
 
         case 4:
         danoJogador = 20;
-        printf("");
+        printf("Mercurius: Usou Dura Lex Sed Lex");
         break;
         
         case 5:
         danoJogador = 0;
         vidaJogador = vidaJogador + 15;
         escudo = 1;
-        printf("");
+        printf("Mercurius: Usou defender e se curou");
         break;
 
         default:
@@ -89,7 +89,7 @@ void batalha(float vidaJogador, float vidaboss, int turno) {
     }
 
 
- /*aplica o dano no Boss*/
+ //aplica o dano no boss
     vidaboss = vidaboss - danoJogador;
 
     printf("\nVoce causou %d de dano!\n", danoJogador);
@@ -103,7 +103,7 @@ void batalha(float vidaJogador, float vidaboss, int turno) {
     }
 
     if (escudo == 1) {
-    printf("\nVoce usou o escudo da Guarda Áurea!\n");
+    printf("\nVoce usou a defesa\n");
     printf("\nVoce recuperou 15 de HP!\n");
     printf("\nO ataque do Boss foi bloqueado!\n");
 
@@ -111,14 +111,14 @@ void batalha(float vidaJogador, float vidaboss, int turno) {
     return;
     }
 
- /*turno do Boss*/
+ //turno do boss
     printf("\nTURNO DO BOSS\n");
-    printf("\nO Boss atacou voce!\n");
-    printf("\nO Boss causou %d de dano!\n", danoBoss);
+    printf("\nO Reinhard atacou voce!\n");
+    printf("\nO Reinhard causou %d de dano!\n", danoBoss);
 
     vidaJogador = vidaJogador - danoBoss;
 
-    /*CASO RECURSIVO: a funcao batalha chama ela mesma para iniciar o proximo turno*/
+    //recursao, chama a propria funcao para comecar um novo turno
     batalha(vidaJogador, vidaboss, turno + 1);
 
 
@@ -129,14 +129,13 @@ int main()
     float vidaJogador = 100;
     float vidaboss = 200;
     
-    printf("\nBOSS BATTLE EM C\n");
-    printf("\nUm Boss apareceu!\n");
+    printf("\nReinard apareceu!\n");
     printf("\nPrepare-se para a batalha!\n");
 
     printf("\nMercurius: Ah… finalmente, o homem que carrega o céu sobre os ombros. ");
     printf("\nReinhard: E você deve ser o homem que se esconde além dele. ");
 
-    /*inicia a batalha no turno 1*/
+    //inicia a batalha no turno 1
     batalha(vidaJogador, vidaboss, 1);
 
     printf("\nFim do jogo.\n");
